78569fdc0f0b8c5ea2aaa07c0d853ee4e5304808
Chekoeko <47895069+Chekoeko@users.noreply.github.com>
2019-04-26 13:13:52 -0400

    Adding Shemo
    Adding Shemo port jeuno

---
+++ scripts/zones/Port_Jeuno/npcs/Shemo.lua
@@ -0,0 +1,42 @@
+-----------------------------------
+-- Area: Port Jeuno
+--  NPC: Shemo
+-- Seal trader
+-- !pos -55.077 0.000 11.302
+-----------------------------------
+require("scripts/globals/settings");
+require("scripts/globals/quests");
+local ID = require("scripts/zones/Port_Jeuno/IDs");
+-----------------------------------
+
+function onTrade(player,npc,trade)
+	if ((trade:hasItemQty(1126,3))) then
+		count = trade:getItemCount();
+		Beastmensseal = trade:hasItemQty(1126,3);
+		if (Beastmensseal == true and count == 3) then
+		    player:tradeComplete();
+			player:addItem(1127)
+		end
+	elseif ((trade:hasItemQty(1127,3))) then
+		count = trade:getItemCount();
+		Beastmensseal = trade:hasItemQty(1127,3);
+		if (Beastmensseal == true and count == 3) then
+		    player:tradeComplete();
+			player:addItem(2955)
+		end
+	elseif ((trade:hasItemQty(2955,3))) then
+		count = trade:getItemCount();
+		Beastmensseal = trade:hasItemQty(2955,3);
+		if (Beastmensseal == true and count == 3) then
+		    player:tradeComplete();
+			player:addItem(2956)
+		end
+	elseif ((trade:hasItemQty(2956,3))) then
+		count = trade:getItemCount();
+		Beastmensseal = trade:hasItemQty(2956,3);
+		if (Beastmensseal == true and count == 3) then
+		    player:tradeComplete();
+			player:addItem(2957)
+		end
+	end
+end; 
